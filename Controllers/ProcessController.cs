325d6007c914a95e2a8114a79b36db18f59809b3
molvera1992 <sistemas_olverapille@hotmail.com>
2023-06-19 23:01:25 -0600

    Agregando nuevos ActionResult

--- Controllers/ProcessController.cs
+++ Controllers/ProcessController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IRS.Models;
 using IRS.Models.ViewModels;
+
+
 /// <summary>
 /// M Olvera and J Montoya are in charge
 /// </summary>
@@ -12,24 +15,19 @@ namespace IRS.Controllers
     public class ProcessController : Controller
     {
         // GET: Process
-        public ActionResult Ejemplo_Formulario()
+        public ActionResult Declaration_Errors()
         {
             return View();
         }
-        public ActionResult Saludo(Saludo sal)
-        {
-            if (!ModelState.IsValid)
-            {
-                ViewBag.Message = "No mms verifica tus datos";
-                return View("Ejemplo_Formulario");
-            }
-            return View(sal);
-        }
-
-        public ActionResult Downtime_Entry ()
+        public ActionResult Downtime_Entry()
         {
             return View();
         }
 
+        public ActionResult Kiosk_Configuration()
+        {
+            return View();
+        }
+  
     }
 }
