18433090fa62ca3fa1f6adfd12686aa6dbfab3e7
molvera1992 <sistemas_olverapille@hotmail.com>
2023-06-20 22:42:51 -0600

    Se agregaron varias Vistas

--- Controllers/ProcessController.cs
+++ Controllers/ProcessController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Web.Mvc;
 using IRS.Models;
@@ -14,20 +15,20 @@ namespace IRS.Controllers
 {
     public class ProcessController : Controller
     {
-        // GET: Process
-        public ActionResult Declaration_Errors()
-        {
-            return View();
-        }
-        public ActionResult Downtime_Entry()
-        {
-            return View();
-        }
+        // Controllers para Vistas dentro de folder PROCESS
+        public ActionResult Equipment_Repair() { return View(); }
 
-        public ActionResult Kiosk_Configuration()
-        {
-            return View();
-        }
+        public ActionResult Kamishibai_Board() { return View(); }
+
+        public ActionResult Audit_Controls () { return View(); }
+        public ActionResult Valor_Apps() { return View(); }
+
+        // Controllers para Vistas dentro de folder PROCESS - > VALOR
+        public ActionResult Declaration_Errors() {return View(); }
+ 
+        public ActionResult Downtime_Entry() { return View(); }
+       
+        public ActionResult Kiosk_Configuration() { return View(); }
   
     }
 }
