ef22449dedcff7336570abec822b830b34c5554e
Miguel Olvera <sistemas_olverapille@hotmail.com>
2024-07-24 12:19:34 -0600

    Test

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,7 +1,12 @@
 ﻿using IRS.Models;
+using IRS.Models.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity.Core.Objects;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Management;
 using System.Web.Mvc;
@@ -23,14 +28,65 @@ namespace IRS.Controllers
         }
 
         [HttpPost]
-        public ActionResult Registrar(ITDBEntities obj_usuarios)
+        public ActionResult Registrar(CBSEntities1 obj_usuarios, Usuario_confirmar confirmar)
         {
-            bool registradro;
-            string mensaje;
-            var model = obj_usuarios.Users.ToList();
+            bool registrado;
+            string mensaje = "", password = "dfdsf", mail = "dsfdsfsd";
 
+            var dato = ( from x in obj_usuarios.Usuarios
+                         where x.Password == confirmar.ConfirmarClave
+                         select new
+                         {
+                             x.Password,
+                             x.Id_Usuario
 
-            return View();
+                         }).FirstOrDefault();
+
+            if(dato.Password == confirmar.ConfirmarClave )
+            {
+                password = ConvertirSha256(dato.Password);
+            }
+            else
+            {
+                ViewData["Mensaje"] = "Las contraseñas no coinciden";
+                return View();
+            }
+
+            try
+            {
+                CBSEntities1 cb = new CBSEntities1();
+                ObjectParameter obj_registrado = new ObjectParameter("Registrado", typeof(bool));
+                ObjectParameter obj_message = new ObjectParameter("Message", typeof(string));
+                cb.sp_RegistrarUsuarios(mail, password, obj_registrado, obj_message);
+                cb.SaveChanges();
+
+                registrado = Convert.ToBoolean(obj_registrado.Value);
+                mensaje = obj_message.Value.ToString();
+
+                ViewData["Mensaje"] = mensaje;
+
+                return View();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            
+            
+        }
+
+        public static string ConvertirSha256(string texto)
+        {
+            StringBuilder sb = new StringBuilder();
+            using (SHA256 hash = SHA256Managed.Create()) 
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(texto));
+
+                foreach(byte b in result ) 
+                    sb.Append(b.ToString("x2"));
+            }
+            return sb.ToString();
         }
     }
 }
---
+++ Models/ViewModels/Usuario_confirmar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IRS.Models.ViewModels
+{
+    public class Usuario_confirmar
+    {
+        public string ConfirmarClave { get; set; }
+    }
+}
